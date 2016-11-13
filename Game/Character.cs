9fd34f33a363e7c2402244e32d46e522d4d306fd
kolosovakaterina26 <kolosovakaterina26@yande.ru>
2016-11-13 20:18:54 +0300

    First Character

---
+++ Game/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Game
+{
+    class Character
+    {
+        int x, y;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public Size sizeOfCharacter;
+        Size sizeOfForm;
+        public int[] speeds;
+        public bool[] keysPressed;
+        public Character(int x, int y, Size sizeOfForm)
+        {
+            this.x = x;
+            this.y = y;
+            this.sizeOfForm = sizeOfForm;
+
+            sizeOfCharacter = new Size(50, 50);
+            speeds = new int[2];
+            speeds[0] = 2;
+            speeds[1] = 4;
+            keysPressed = new bool[8];
+        }
+    }
+}
